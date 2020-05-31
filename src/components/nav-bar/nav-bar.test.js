8e1d088296bb9f4f6ec8cb3fe0a4b2dedf0285c0
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-05-31 15:44:10 -0500

    nav-bar.test updated with testing-library/react

--- src/components/nav-bar/nav-bar.test.js
+++ src/components/nav-bar/nav-bar.test.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render, screen } from '@testing-library/react'
 
 import NavBar from './'
 
 describe('NavBar', () => {
-  let root = null
-
   beforeEach(() => {
     const props = {
       social: {
@@ -16,26 +14,22 @@ describe('NavBar', () => {
       location: { pathname: '' },
     }
 
-    root = renderer.create(<NavBar {...props} />).root
+    render(<NavBar {...props} />).root
   })
 
   describe('when NavBar is mounted', () => {
-    it('should renders correctly', () => {
-      expect(root).toBeTruthy()
-    })
+    it('should renders the blog navigation links', async () => {
+      const blogLinkText = await screen.findByText('Blog')
+      const aboutLinkText = await screen.findByText('Sobre el autor')
+      const youtubeLinkText = await screen.findByText('YouTube')
+      const facebookLinkText = await screen.findByText('Facebook')
+      const githubLinkText = await screen.findByText('Github')
 
-    it('should renders the blog navigation links', () => {
-      const blogLinkText = root.findByProps({ 'data-testid': 'link-blog' }).children[0].props.children
-      const aboutLinkText = root.findByProps({ 'data-testid': 'link-about' }).children[0].props.children
-      const youtubeLinkText = root.findByProps({ 'data-testid': 'link-social-youtube' }).children[0]
-      const facebookLinkText = root.findByProps({ 'data-testid': 'link-social-facebook' }).children[0]
-      const githubLinkText = root.findByProps({ 'data-testid': 'link-social-github' }).children[0]
-
-      expect(blogLinkText).toBe('Blog')
-      expect(aboutLinkText).toBe('Sobre el autor')
-      expect(youtubeLinkText).toBe('YouTube')
-      expect(facebookLinkText).toBe('Facebook')
-      expect(githubLinkText).toBe('Github')
+      expect(blogLinkText).toBeTruthy()
+      expect(aboutLinkText).toBeTruthy()
+      expect(youtubeLinkText).toBeTruthy()
+      expect(facebookLinkText).toBeTruthy()
+      expect(githubLinkText).toBeTruthy()
     })
   })
 })
