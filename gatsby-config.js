fd8aaac22e0296fc8cc88e5fb81e8fd4022938fd
Juan Correa <juan-carlos-correa@users.noreply.github.com>
2020-07-01 00:07:45 -0500

    feature/material-ui (#5)
    * favicon updated
    
    * Feature/material-ui (#4)
    
    * added material ui
    
    * added material theme
    
    * reestructure layout component
    
    * added initial landing page and blog pages
    
    * updated Logo and Footer components
    
    * theme provided and navbar component updated
    
    * LandingPageContainer added
    
    * updated LandingPageContainer content
    
    * added styles prop to Footer
    
    * reestructured blog page
    
    * blog-post template updated
    
    * about page updated
    
    * 404 page updaed
    
    * removed typography dependencies
    
    * removed eslint warning on 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -72,12 +73,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/components/landing-page-container/index.js
@@ -0,0 +1,3 @@
+import { LandingPageContainer } from './landing-page-container'
+
+export { LandingPageContainer }
