1d51dfe621636879c1f1d19b5012473e318ae3c7
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-09-30 17:31:36 -0500

    added swipeable drawer element on xs screen sizes

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link as GatsbyLink } from "gatsby"
 import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -6,6 +6,11 @@ import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
 import withWidth from '@material-ui/core/withWidth'
 import Grid from '@material-ui/core/Grid'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ListItem from '@material-ui/core/ListItem'
+import List from '@material-ui/core/List'
 import PropTypes from 'prop-types'
 
 const useStyles = makeStyles((theme) => ({
@@ -46,6 +51,42 @@ const navBarLinks = [
 
 const NavBar = ({ location, width }) => {
   const classes = useStyles()
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const renderLinks = () => (
+    navBarLinks.map(({ path, label}) => {
+      const className = `${classes.toolbarLink} ${location.pathname === path && classes.toolbarLinkSelected}`
+      return (
+        <Grid item key={label}>
+          <Link
+            component={GatsbyLink}
+            to={path}
+            className={className}>
+              {label}
+          </Link>
+        </Grid>
+      )
+    })
+  )
+
+  const renderDrawIcon = () => (
+    <IconButton
+      color="primary"
+      aria-label="open drawer"
+      onClick={handleDrawerOpen}
+      edge="end"
+    >
+      <MenuIcon />
+    </IconButton>
+  )
 
   return (
     <Toolbar component="nav" variant="dense" className={classes.toolbar}>
@@ -64,22 +105,29 @@ const NavBar = ({ location, width }) => {
         <Grid item>
           <Grid container spacing={1}>
           {
-            navBarLinks.map(({ path, label}) => {
-              const className = `${classes.toolbarLink} ${location.pathname === path && classes.toolbarLinkSelected}`
-              return (
-                <Grid item key={label}>
-                  <Link
-                    component={GatsbyLink}
-                    to={path}
-                    className={className}>
-                      {label}
-                  </Link>
-                </Grid>
-              )
-            })
+            width === 'xs' ? renderDrawIcon() : renderLinks()
           }
           </Grid>
         </Grid>
+
+        <SwipeableDrawer
+          anchor="left"
+          open={open}
+          onOpen={handleDrawerOpen}
+          onClose={handleDrawerClose}
+        >
+          <List>
+            {
+              navBarLinks.map(({ path, label }) => (
+                <ListItem button key={label}>
+                  <Link to={path} component={GatsbyLink}>
+                    {label}
+                  </Link>
+                </ListItem>
+              ))
+            }
+          </List>
+        </SwipeableDrawer>
       </Grid>
     </Toolbar>
   )
