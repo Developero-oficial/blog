7a2de450c71ece36bbaa5ea015044668703e9c6f
Juan Correa <juan-carlos-correa@users.noreply.github.com>
2020-09-30 17:47:33 -0500

    Navbar styles updated (#14)
    * favicon updated
    
    * Feature/material-ui (#4)
    
    * added material ui
    
    * added material theme
    
    * reestructure layout component
    
    * added initial landing page and blog pages
    
    * updated Logo and Footer components
    
    * theme provided and navbar component updated
    
    * LandingPageContainer added
    
    * updated LandingPageContainer content
    
    * added styles prop to Footer
    
    * reestructured blog page
    
    * blog-post template updated
    
    * about page updated
    
    * 404 page updaed
    
    * removed typography dependencies
    
    * removed eslint warning on 404 page
    
    * php-mvc-framework-tutorial post added (#6)
    
    * php-mvc-framework-tutorial post added
    
    * updated package.json
    
    * Post/react basic tutorial (#8)
    
    * php-mvc-framework-tutorial post added (#7)
    
    * favicon updated
    
    * Feature/material-ui (#4)
    
    * added material ui
    
    * added material theme
    
    * reestructure layout component
    
    * added initial landing page and blog pages
    
    * updated Logo and Footer components
    
    * theme provided and navbar component updated
    
    * LandingPageContainer added
    
    * updated LandingPageContainer content
    
    * added styles prop to Footer
    
    * reestructured blog page
    
    * blog-post template updated
    
    * about page updated
    
    * 404 page updaed
    
    * removed typography dependencies
    
    * removed eslint warning on 404 page
    
    * php-mvc-framework-tutorial post added (#6)
    
    * php-mvc-framework-tutorial post added
    
    * updated package.json
    
    * react-basic-tutorial added
    
    * Feature/minor updates blog (#9)
    
    * php-mvc-framework-tutorial post added (#7)
    
    * favicon updated
    
    * Feature/material-ui (#4)
    
    * added material ui
    
    * added material theme
    
    * reestructure layout component
    
    * added initial landing page and blog pages
    
    * updated Logo and Footer components
    
    * theme provided and navbar component updated
    
    * LandingPageContainer added
    
    * updated LandingPageContainer content
    
    * added styles prop to Footer
    
    * reestructured blog page
    
    * blog-post template updated
    
    * about page updated
    
    * 404 page updaed
    
    * removed typography dependencies
    
    * removed eslint warning on 404 page
    
    * php-mvc-framework-tutorial post added (#6)
    
    * php-mvc-framework-tutorial post added
    
    * updated package.json
    
    * added spacing on lit posts
    
    * divider margin updated
    
    * cover image added on post content
    
    * Feature/social share (#11)
    
    * php-mvc-framework-tutorial post added (#7)
    
    * favicon updated
    
    * Feature/material-ui (#4)
    
    * added material ui
    
    * added material theme
    
    * reestructure layout component
    
    * added initial landing page and blog pages
    
    * updated Logo and Footer components
    
    * theme provided and navbar component updated
    
    * LandingPageContainer added
    
    * updated LandingPageContainer content
    
    * added styles prop to Footer
    
    * reestructured blog page
    
    * blog-post template updated
    
    * about page updated
    
    * 404 page updaed
    
    * removed typography dependencies
    
    * removed eslint warning on 404 page
    
    * php-mvc-framework-tutorial post added (#6)
    
    * php-mvc-framework-tutorial post added
    
    * updated package.json
    
    * social-share added and post layout updated
    
    * Navbar reestructure (#13)
    
    * updated navbar styles and brand logo
    
    * added swipeable drawer element on xs screen sizes

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,50 +1,135 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link as GatsbyLink } from "gatsby"
 import { makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
+import withWidth from '@material-ui/core/withWidth'
+import Grid from '@material-ui/core/Grid'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ListItem from '@material-ui/core/ListItem'
+import List from '@material-ui/core/List'
 import PropTypes from 'prop-types'
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
-    paddingBottom: theme.spacing(1),
-    borderBottom: `1px solid ${theme.palette.divider}`,
-    justifyContent: 'space-between',
-    overflowX: 'auto',
+    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(2),
+  },
+  brandLink: {
+    '&:hover': {
+      textDecoration: 'none',
+    }
   },
   toolbarLink: {
     padding: theme.spacing(1),
     flexShrink: 0,
+    borderRadius: 5,
+    transition: '0.3s',
+    '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      textDecoration: 'none',
+    }
   },
   toolbarLinkSelected: {
-    color: theme.palette.secondary.main
+    backgroundColor: 'rgba(0, 0, 0, 0.1)'
   },
 }));
 
-const NavBar = ({ renderLogo, location, links }) => {
+const navBarLinks = [
+  {
+    path: '/blog',
+    label: 'Blog',
+  },
+  {
+    path: '/about',
+    label: 'Acerca De',
+  },
+]
+
+const NavBar = ({ location, width }) => {
   const classes = useStyles()
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const renderLinks = () => (
+    navBarLinks.map(({ path, label}) => {
+      const className = `${classes.toolbarLink} ${location.pathname === path && classes.toolbarLinkSelected}`
+      return (
+        <Grid item key={label}>
+          <Link
+            component={GatsbyLink}
+            to={path}
+            className={className}>
+              {label}
+          </Link>
+        </Grid>
+      )
+    })
+  )
+
+  const renderDrawIcon = () => (
+    <IconButton
+      color="primary"
+      aria-label="open drawer"
+      onClick={handleDrawerOpen}
+      edge="end"
+    >
+      <MenuIcon />
+    </IconButton>
+  )
 
   return (
-    <AppBar position="static" color="transparent">
-      <Toolbar component="nav" variant="dense" className={classes.toolbar}>
-          {renderLogo()}
+    <Toolbar component="nav" variant="dense" className={classes.toolbar}>
+      <Grid
+        container
+        alignItems="center"
+        alignContent="center"
+        justify="space-between">
+        <Grid item xs={2}>
+          <Typography variant="h5" variantMapping={{ h5: 'h1' }}>
+            <Link to="/" component={GatsbyLink} className={classes.brandLink}>
+              Developero
+            </Link>
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Grid container spacing={1}>
+          {
+            width === 'xs' ? renderDrawIcon() : renderLinks()
+          }
+          </Grid>
+        </Grid>
 
-          <div>
+        <SwipeableDrawer
+          anchor="left"
+          open={open}
+          onOpen={handleDrawerOpen}
+          onClose={handleDrawerClose}
+        >
+          <List>
             {
-              links.map(({ path, label}) => (
-                <Link
-                  key={label}
-                  component={GatsbyLink}
-                  to={path}
-                  className={`${classes.toolbarLink} ${location.pathname === path && classes.toolbarLinkSelected}`}>
+              navBarLinks.map(({ path, label }) => (
+                <ListItem button key={label}>
+                  <Link to={path} component={GatsbyLink}>
                     {label}
-                </Link>
+                  </Link>
+                </ListItem>
               ))
             }
-          </div>
-      </Toolbar>
-    </AppBar>
+          </List>
+        </SwipeableDrawer>
+      </Grid>
+    </Toolbar>
   )
 }
 
@@ -52,11 +137,7 @@ NavBar.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }),
-  links: PropTypes.arrayOf(PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-  })).isRequired,
-  renderLogo: PropTypes.func.isRequired,
+  width: PropTypes.string.isRequired,
 }
 
-export default NavBar
+export default withWidth()(NavBar)
