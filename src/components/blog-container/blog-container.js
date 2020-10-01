a38eceddaba3647199061650f198c1cbf2edc541
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-10-01 10:23:01 -0500

    updated blog page

--- src/components/blog-container/blog-container.js
+++ src/components/blog-container/blog-container.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Typography from "@material-ui/core/Typography"
 import Grid from "@material-ui/core/Grid"
 import { makeStyles } from "@material-ui/core/styles"
 
@@ -19,31 +18,12 @@ export const BlogContainer = ({ posts }) => {
   const classes = useStyles()
 
   return (
-    <>
-      <Grid item xs={12} className={classes.mainContent}>
-        <Typography variant="h3" component="h1" align="center" gutterBottom>
-          Developero Blog
-        </Typography>
-
-        <Typography
-          variant="body1"
-          component="h3"
-          color="textPrimary"
-          align="center"
-          gutterBottom
-        >
-          Navega por las publicaciones más recientes sobre desarrollo de
-          software.
-        </Typography>
-      </Grid>
-
-      <Grid container className={classes.postsContainer} spacing={3}>
-        {posts.map(({ node }) => (
-          <Grid item xs={12} md={4} key={node.fields.slug}>
-            <BlogListItem node={node} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <Grid container className={classes.postsContainer} spacing={3}>
+      {posts.map(({ node }) => (
+        <Grid item xs={12} md={6} key={node.fields.slug}>
+          <BlogListItem node={node} />
+        </Grid>
+      ))}
+    </Grid>
   )
 }
--- src/components/blog-list-item/blog-list-item.js
+++ src/components/blog-list-item/blog-list-item.js
@@ -5,15 +5,34 @@ import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import CardMedia from "@material-ui/core/CardMedia"
 import CardHeader from "@material-ui/core/CardHeader"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(() => ({
+  card: {
+    backgroundColor: "#F3F3EE",
+    borderRadius: 5,
+    boxShadow: "none",
+  },
+  link: {
+    textDecoration: "none",
+  },
+}))
 
 export const BlogListItem = ({ node }) => {
+  const classes = useStyles()
   const title = node.frontmatter.title || node.fields.slug
 
   return (
     <article>
-      <Card>
+      <Card classes={{ root: classes.card }}>
         <CardHeader
-          title={<Link to={node.fields.slug}>{title}</Link>}
+          title={
+            <Link to={node.fields.slug} className={classes.link}>
+              <Typography variant="h5" component="h2" color="primary">
+                {title}
+              </Typography>
+            </Link>
+          }
           subheader={<small>{node.frontmatter.date}</small>}
         />
         <Link to={node.fields.slug}>
