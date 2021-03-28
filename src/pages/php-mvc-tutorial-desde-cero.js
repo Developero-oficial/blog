4d313e404c14504d3557f20bd4994aace867b8f9
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-03-28 14:21:41 -0600

    added initial structure

--- src/pages/php-mvc-tutorial-desde-cero.js
+++ src/pages/php-mvc-tutorial-desde-cero.js
@@ -1,7 +1,60 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
 
-const PhpTutorialDesdeCero = () => {
-  return <h1>PhpTutorialDesdeCero</h1>
+import { Layout } from "../components/layout"
+import { SEO } from "../components/seo"
+
+const getUri = ({ location, siteUrl }) => {
+  return `${siteUrl}${location.pathname}`
+}
+
+const PhpTutorialDesdeCero = ({ data, location }) => {
+  const { siteUrl } = data.site.siteMetadata
+  return (
+    <>
+      <SEO
+        title="Php MVC Tutorial desde cero - Developero"
+        description=""
+        url={getUri({ location, siteUrl })}
+      />
+      <Layout location={location}>
+        <Grid container spacing={2} direction="column">
+          <Grid item>
+            <Box my={4}>
+              <Typography
+                variant="h4"
+                component="h1"
+                gutterBottom
+                color="primary"
+              >
+                PHP con MVC tutorial desde cero Gratis
+              </Typography>
+            </Box>
+          </Grid>
+
+          <Grid item>
+            <Typography variant="h6" component="p" gutterBottom>
+              Bienvenido al curso gratuito de Php con MVC en el que aprenderás a
+              crear tu propio mini framework usando php vanila (puro).
+            </Typography>
+          </Grid>
+        </Grid>
+      </Layout>
+    </>
+  )
 }
 
 export default PhpTutorialDesdeCero
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`
