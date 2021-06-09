51384cdbd9f069ebdc71f7c7a9a08fe8fc6c8b0f
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-06-08 20:50:54 -0500

    added blog page accessibility test

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,11 +1,17 @@
 /// <reference types="Cypress" />
 
 describe("Accessibility tests", () => {
-  beforeEach(() => {
+  // beforeEach(() => {
+  //   cy.visit("/").get("main").injectAxe()
+  // })
+
+  it("Has no detectable accessibility violations on load on main page", () => {
     cy.visit("/").get("main").injectAxe()
+    cy.checkA11y()
   })
 
-  it("Has no detectable accessibility violations on load", () => {
+  it("Has no detectable accessibility violations on load on blog page", () => {
+    cy.visit("/blog").get("main").injectAxe()
     cy.checkA11y()
   })
 })
