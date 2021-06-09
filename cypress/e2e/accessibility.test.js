0190955d5781fbcfe0d4eacc21e5d8f2a61531f4
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-06-08 21:00:58 -0500

    added accessibility tests for rest of main pages

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,10 +1,6 @@
 /// <reference types="Cypress" />
 
 describe("Accessibility tests", () => {
-  // beforeEach(() => {
-  //   cy.visit("/").get("main").injectAxe()
-  // })
-
   it("Has no detectable accessibility violations on load on main page", () => {
     cy.visit("/").get("main").injectAxe()
     cy.checkA11y()
@@ -12,6 +8,21 @@ describe("Accessibility tests", () => {
 
   it("Has no detectable accessibility violations on load on blog page", () => {
     cy.visit("/blog").get("main").injectAxe()
+    cy.checkA11y()
+  })
+
+  it("Has no detectable accessibility violations on load on ebooks page", () => {
+    cy.visit("/ebooks").get("main").injectAxe()
+    cy.checkA11y()
+  })
+
+  it("Has no detectable accessibility violations on load on courses premium page", () => {
+    cy.visit("/courses-premium").get("main").injectAxe()
+    cy.checkA11y()
+  })
+
+  it.only("Has no detectable accessibility violations on load on about page", () => {
+    cy.visit("/about").get("main").injectAxe()
     cy.checkA11y()
   })
 })
