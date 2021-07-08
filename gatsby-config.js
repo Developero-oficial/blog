255494645e3c1462a64e9acc57a1c60ab62783dc
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-07-08 10:15:56 -0500

    udpated coupons web-developero-7

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,9 +22,9 @@ module.exports = {
     },
     premiumCourses: {
       tddReact:
-        "https://www.udemy.com/course/tdd-react-js/?couponCode=WEB-DEVELOPERO-6",
+        "https://www.udemy.com/course/tdd-react-js/?couponCode=WEB-DEVELOPERO-7",
       reactPatterns:
-        "https://www.udemy.com/course/react-js-patrones/?couponCode=WEB-DEVELOPERO-6",
+        "https://www.udemy.com/course/react-js-patrones/?couponCode=WEB-DEVELOPERO-7",
     },
     ebooks: {
       hooksManual: "https://amzn.to/3o8zEM2",
